43c0cd9bd0e6d181d2aaca53a9e8141acf8377f9
eleanormark <eleanormark@gmail.com>
2013-11-13 16:56:03 -0800

    remove code duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   has_many :votes
   has_many :comments
   validates :name, :presence => true
-  validates :email, :presence => true
-  validates_uniqueness_of :email
+  validates :email, :presence => true, :uniqueness => true
+
   has_secure_password
-  validates_uniqueness_of :email
 end
