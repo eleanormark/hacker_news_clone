06f2c2c3cd077667b40e2911afca7138e49b9314
eleanormark <eleanormark@gmail.com>
2013-11-13 16:54:25 -0800

    change tabs to spaces

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,9 @@
 class VotesController < ApplicationController
   def create
-  	@vote = current_user.votes.where(:post_id => params[:vote][:post_id]).first
-  	@vote ||= current_user.votes.create(vote_params)
-  	@vote.update(:up => params[:vote][:up])
-  	redirect_to :back
+    @vote = current_user.votes.where(:post_id => params[:vote][:post_id]).first
+    @vote ||= current_user.votes.create(vote_params)
+    @vote.update(:up => params[:vote][:up])
+    redirect_to :back
   end
 
   private
