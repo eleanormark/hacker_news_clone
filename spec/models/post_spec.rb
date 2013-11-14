9972633038792761ea317a217b75dc481d37ce3f
eleanormark <eleanormark@gmail.com>
2013-11-13 16:51:11 -0800

    uncomment spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,5 @@ describe Post do
   it { should validate_presence_of :title }
   it { should validate_presence_of :url }
   it { should belong_to :user }
-  # it { should have_many :votes }
+  it { should have_many :votes }
 end
